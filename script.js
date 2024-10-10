619d0d0bb8a4073d24114daaea11160a2da9fc0c
Gideon kirui <kipyegongideon99@gmail.com>
2024-10-10 18:14:51 +0300

    repo

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const searchBtn = document.getElementById('search-btn');
 const mealList = document.getElementById('meal');
 const mealDetailsContent = document.querySelector('.meal-details-content');
 const recipeCloseBtn = document.getElementById('recipe-close-btn');
+document.getElementById('meal').classList.add('loaded');
+
 
 // event listeners
 searchBtn.addEventListener('click', getMealList);
