6ac408c94be9d4ca9c14684919e739adecbaaa8e
Gideon kirui <kipyegongideon99@gmail.com>
2024-10-11 09:37:36 +0300

    update

--- script.js
+++ script.js
@@ -4,78 +4,91 @@ const mealDetailsContent = document.querySelector('.meal-details-content');
 const recipeCloseBtn = document.getElementById('recipe-close-btn');
 document.getElementById('meal').classList.add('loaded');
 
-
 // event listeners
 searchBtn.addEventListener('click', getMealList);
 mealList.addEventListener('click', getMealRecipe);
 recipeCloseBtn.addEventListener('click', () => {
-    mealDetailsContent.parentElement.classList.remove('showRecipe');
+    const mealDetails = document.querySelector('.meal-details');
+    if (mealDetails) {
+        mealDetails.style.display = 'none';
+        mealDetails.classList.remove('showRecipe'); 
+    }
 });
 
-
-function getMealList(){
+// get meal list from API
+function getMealList() {
     let searchInputTxt = document.getElementById('search-input').value.trim();
     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
-    .then(response => response.json())
-    .then(data => {
-        let html = "";
-        if(data.meals){
-            data.meals.forEach(meal => {
-                html += `
-                    <div class = "meal-item" data-id = "${meal.idMeal}">
-                        <div class = "meal-img">
-                            <img src = "${meal.strMealThumb}" alt = "food">
+        .then(response => response.json())
+        .then(data => {
+            let html = "";
+            if (data.meals) {
+                data.meals.forEach(meal => {
+                    html += `
+                        <div class = "meal-item" data-id = "${meal.idMeal}">
+                            <div class = "meal-img">
+                                <img src = "${meal.strMealThumb}" alt = "food">
+                            </div>
+                            <div class = "meal-name">
+                                <h3>${meal.strMeal}</h3>
+                                <a href = "#" class = "recipe-btn">Get Recipe</a>
+                            </div>
                         </div>
-                        <div class = "meal-name">
-                            <h3>${meal.strMeal}</h3>
-                            <a href = "#" class = "recipe-btn">Get Recipe</a>
-                        </div>
-                    </div>
-                `;
-            });
-            mealList.classList.remove('notFound');
-        } else{
-            html = "Sorry, we didn't find any meal!";
-            mealList.classList.add('notFound');
-        }
+                    `;
+                });
+                mealList.classList.remove('notFound');
+            } else {
+                html = "Sorry, we didn't find any meal!";
+                mealList.classList.add('notFound');
+            }
 
-        mealList.innerHTML = html;
-    });
+            mealList.innerHTML = html;
+        });
 }
 
-
-// get recipe of the meal
-function getMealRecipe(e){
+function getMealRecipe(e) {
     e.preventDefault();
-    if(e.target.classList.contains('recipe-btn')){
+    if (e.target.classList.contains('recipe-btn')) {
         let mealItem = e.target.parentElement.parentElement;
         fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
-        .then(response => response.json())
-        .then(data => mealRecipeModal(data.meals));
+            .then(response => response.json())
+            .then(data => showRecipeDetails(data.meals[0]));
     }
 }
 
-function mealRecipeModal(meal) {
-    console.log(meal);
-    meal = meal[0];
+function showRecipeDetails(meal) {
+    let ingredients = [];
+    for (let i = 1; i <= 20; i++) {
+        const ingredient = meal[`strIngredient${i}`];
+        const measure = meal[`strMeasure${i}`];
+        if (ingredient) {
+            ingredients.push(`${measure} ${ingredient}`);
+        }
+    }
 
-    let html = `
-        <h2 class = "recipe-title">${meal.strMeal}</h2>
-        <p class = "recipe-category">${meal.strCategory}</p>
-        <div class = "recipe-instruct">
-            <h3>Instructions:</h3>
-            <p>${meal.strInstructions}</p>
-        </div>
-        <div class = "recipe-meal-img">
-            <img src = "${meal.strMealThumb}" alt = "">
-        </div>
-        <div class = "recipe-link">
-            <a href = "${meal.strYoutube}" target = "_blank">Watch Video</a>
-        </div>
-    `;
+  
+const ingredientList = ingredients.map(ingredient => `<li>${ingredient}</li>`).join('');
 
-    mealDetailsContent.innerHTML = html;
 
-    // Trigger the fade-in effect by adding 'showRecipe' class
-    mealDetailsContent.parentElement.classList.add('showRecipe');
+const mealDetailsContent = `
+    <h2 class="recipe-title">${meal.strMeal}</h2>
+    <p class="recipe-category">${meal.strCategory}</p>
+    <div class="recipe-instruct">
+        <h3>Ingredients:</h3>
+        <ol>${ingredientList}</ol> <!-- Changed to <ol> for numbering -->
+        <h3>Instructions:</h3>
+        <p>${meal.strInstructions}</p>
+    </div>
+    <div class="recipe-meal-img">
+        <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+    </div>
+    <div class="recipe-link">
+        <a href="${meal.strYoutube}" target="_blank">Watch Video</a>
+    </div>
+`;
+
+
+    document.querySelector('.meal-details-content').innerHTML = mealDetailsContent;
+
+    document.querySelector('.meal-details').style.display = 'block';
 }
